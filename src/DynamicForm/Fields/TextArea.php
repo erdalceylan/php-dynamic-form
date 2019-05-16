6696552a327609ff5f1700e188caa0f2ad69fc2b
Erdal CEYLAN <erdalceylan25@gmail.com>
2019-05-16 12:06:37 +0300

    extend from Text

--- src/DynamicForm/Fields/TextArea.php
+++ src/DynamicForm/Fields/TextArea.php
@@ -14,49 +14,7 @@ use DynamicForm\Field;
  * Class TextArea
  * @package DynamicForm\Fields
  */
-class TextArea extends Field
+class TextArea extends Text
 {
-    /**
-     * @var string
-     */
-    protected $value;
 
-    /**
-     * @return string
-     */
-    public function getValue(): string
-    {
-        return $this->value;
-    }
-
-    /**
-     * @param string $value
-     * @return static
-     */
-    public function setValue(string $value): self
-    {
-        $this->value = $value;
-        return $this;
-    }
-
-    /**
-     * @param $value
-     * @return bool
-     */
-    public function contain($value): bool
-    {
-        return true;
-    }
-
-    /**
-     * Specify data which should be serialized to JSON
-     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
-     * @return mixed data which can be serialized by <b>json_encode</b>,
-     * which is a value of any type other than a resource.
-     * @since 5.4.0
-     */
-    public function jsonSerialize()
-    {
-        return get_object_vars($this);
-    }
 }
